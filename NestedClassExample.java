ae88810edf24f1353b0ea00ebf3fcdde6a51f1a3
kokihoon <rhrlgns777@gmail.com>
2018-01-05 03:06:24 +0900

    NestedClass

---
+++ NestedClassExample.java
@@ -0,0 +1,8 @@
+public class NestedClassExample {
+    public static void main(String[] args) {
+        Car myCar = new Car();
+
+        Car.Tire tire = myCar.new Tire();
+        Car.Engine engine = new Car.Engine();
+    }
+}
