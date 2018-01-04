5d8df033d523c2690fbec1ac6b8e571877558152
kokihoon <rhrlgns777@gmail.com>
2018-01-05 00:23:17 +0900

    interface

--- KumhoTire.java
+++ KumhoTire.java
@@ -1,18 +1,18 @@
 public class KumhoTire extends Tire{
-    public KumhoTire(String location, int maxRotation) {
-        super(location, maxRotation);
-    }
-
-    @Override
-    public boolean roll() {
-        ++accumulatedRotation;
-        if(accumulatedRotation < maxRotation) {
-            System.out.println(location + " HankookTire 수명: " + (maxRotation - accumulatedRotation) + "회");
-            return true;
-        }
-        else {
-            System.out.println("*** " + location + " HankookTire 펑크 ***");
-            return false;
-        }
-    }
+//    public KumhoTire(String location, int maxRotation) {
+//        super(location, maxRotation);
+//    }
+//
+//    @Override
+//    public boolean roll() {
+//        ++accumulatedRotation;
+//        if(accumulatedRotation < maxRotation) {
+//            System.out.println(location + " HankookTire 수명: " + (maxRotation - accumulatedRotation) + "회");
+//            return true;
+//        }
+//        else {
+//            System.out.println("*** " + location + " HankookTire 펑크 ***");
+//            return false;
+//        }
+//    }
 }
