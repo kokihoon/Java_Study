f3e367ccbd408e7031c4340db15a755d7e8871bb
kokihoon <rhrlgns777@gmail.com>
2018-01-23 15:00:46 +0900

    thread Runable anonymous class

--- BeepPrintExample2.java
+++ BeepPrintExample2.java
@@ -1,7 +1,19 @@
 public class BeepPrintExample2 {
     public static void main(String[] args) {
-        Runnable beepTask = new BeekTask();
-        Thread thread = new Thread(beepTask);
+//        Runnable beepTask = new BeekTask();
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for(int i = 0; i < 5; i++) {
+                    System.out.println("타타탙타");
+                    try{
+                        Thread.sleep(500);
+                    }catch (Exception e) {
+                        
+                    }
+                }
+            }
+        });
 
         thread.start();
 
