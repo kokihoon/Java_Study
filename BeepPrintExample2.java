2835cdf4ca36cf026bc1ded330c2822b61ded4be
kokihoon <rhrlgns777@gmail.com>
2018-01-23 15:02:31 +0900

    thread Runable lambda

--- BeepPrintExample2.java
+++ BeepPrintExample2.java
@@ -1,16 +1,13 @@
 public class BeepPrintExample2 {
     public static void main(String[] args) {
 //        Runnable beepTask = new BeekTask();
-        Thread thread = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                for(int i = 0; i < 5; i++) {
-                    System.out.println("타타탙타");
-                    try{
-                        Thread.sleep(500);
-                    }catch (Exception e) {
-                        
-                    }
+        Thread thread = new Thread( () -> {
+            for(int i = 0; i < 5; i++) {
+                System.out.println("타타타타");
+                try {
+                    Thread.sleep(500);
+                }catch (Exception e) {
+
                 }
             }
         });
