6696ae7bdcaa03df33c99762a8d0838b712975a1
kokihoon <rhrlgns777@gmail.com>
2018-01-29 16:37:05 +0900

    chaged commiit

--- ThreadExample.java
+++ ThreadExample.java
@@ -1,6 +1,7 @@
 public class ThreadExample {
     public static void main(String[] args){
         Thread thread = new MovieThread();
+        thread.setDaemon(true);
         thread.start();
         try {
             Thread.sleep(1000);
--- ThreadStateExample.java
+++
@@ -1,6 +0,0 @@
-public class ThreadStateExample {
-    public static void main(String[] args) {
-        StatePrintThread statePrintThread = new StatePrintThread(new TargetThread());
-        statePrintThread.start();
-    }
-}
