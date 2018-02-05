b68d8ec3f6d48fcd83361c6f148cef7d98b3a87f
kokihoon-king <rhrlgns777@daum.net>
2018-02-05 15:13:51 +0900

    fxml AnchorPane

--- AppMain.java
+++ AppMain.java
@@ -1,32 +1,14 @@
 import javafx.application.Application;
-import javafx.collections.ObservableList;
-import javafx.geometry.Insets;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.TextField;
-import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
-
-
 
 public class AppMain extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
-        HBox hbox = new HBox();
-        hbox.setPadding(new Insets(10));
-        hbox.setSpacing(10);
-
-        TextField textField = new TextField();
-        textField.setPrefWidth(200);
-
-        Button button = new Button();
-        button.setText("확인");
-
-        ObservableList list = hbox.getChildren();
-        list.add(textField);
-        list.add(button);
-
-        Scene scene = new Scene(hbox);
+        Parent root1 = FXMLLoader.load(getClass().getResource("root.fxml"));
+        Scene scene = new Scene(root1);
 
         primaryStage.setTitle("AppMain");
         primaryStage.setScene(scene);
