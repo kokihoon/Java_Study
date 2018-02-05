9a289c77fd7027489e6c39c613eaa16b6298d815
kokihoon-king <rhrlgns777@daum.net>
2018-02-05 11:15:45 +0900

    init, start, stop App

--- AppMain.java
+++ AppMain.java
@@ -2,11 +2,24 @@ import javafx.application.Application;
 import javafx.stage.Stage;
 
 public class AppMain extends Application {
+    public AppMain() {
+        System.out.println(Thread.currentThread().getName()+": AppMain() 호출");
+    }
+    @Override
+    public void init() throws Exception {
+        System.out.println(Thread.currentThread().getName() +": init() 호출");
+    }
+    @Override
+    public void stop() throws Exception {
+        System.out.println(Thread.currentThread().getName()+": stop() 호출");
+    }
     @Override
     public void start(Stage primaryStage) throws Exception {
+        System.out.println(Thread.currentThread().getName() + ": start() 호출");
         primaryStage.show();
     }
     public static void main(String[] args) {
+        System.out.println(Thread.currentThread().getName()+": main() 호출");
         launch(args);
     }
 }
