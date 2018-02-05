25c51a1c93007da93da0b3fdb7c22fd42d9c850c
kokihoon-king <rhrlgns777@daum.net>
2018-02-05 12:02:52 +0900

    HBox TextField JavaFx

--- AppMain.java
+++ AppMain.java
@@ -1,39 +1,36 @@
 import javafx.application.Application;
-import javafx.application.Platform;
-import javafx.geometry.Pos;
+import javafx.collections.ObservableList;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.layout.VBox;
-import javafx.scene.text.Font;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
+
 
 
 public class AppMain extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
-        VBox root = new VBox();
-        root.setPrefHeight(150);
-        root.setPrefWidth(350);
-        root.setAlignment(Pos.CENTER);
-        root.setSpacing(20);
+        HBox hbox = new HBox();
+        hbox.setPadding(new Insets(10));
+        hbox.setSpacing(10);
 
-        Label label = new Label();
-        label.setText("Hello, JavaFX");
-        label.setFont(new Font(50));
+        TextField textField = new TextField();
+        textField.setPrefWidth(200);
 
         Button button = new Button();
         button.setText("확인");
-        button.setOnAction(event-> Platform.exit());
-        root.getChildren().add(button);
-        root.getChildren().add(label);
 
-        Scene scene = new Scene(root);
+        ObservableList list = hbox.getChildren();
+        list.add(textField);
+        list.add(button);
+
+        Scene scene = new Scene(hbox);
 
         primaryStage.setTitle("AppMain");
         primaryStage.setScene(scene);
         primaryStage.show();
-
     }
     public static void main(String[] args) {
         launch(args);
