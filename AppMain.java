da3643942990da71f19f872f73123f131520446b
kokihoon-king <rhrlgns777@daum.net>
2018-02-05 11:35:14 +0900

    label, button javafx

--- AppMain.java
+++ AppMain.java
@@ -1,25 +1,41 @@
 import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
+
 public class AppMain extends Application {
-    public AppMain() {
-        System.out.println(Thread.currentThread().getName()+": AppMain() 호출");
-    }
-    @Override
-    public void init() throws Exception {
-        System.out.println(Thread.currentThread().getName() +": init() 호출");
-    }
-    @Override
-    public void stop() throws Exception {
-        System.out.println(Thread.currentThread().getName()+": stop() 호출");
-    }
     @Override
     public void start(Stage primaryStage) throws Exception {
-        System.out.println(Thread.currentThread().getName() + ": start() 호출");
+        VBox root = new VBox();
+        root.setPrefHeight(150);
+        root.setPrefWidth(350);
+        root.setAlignment(Pos.CENTER);
+        root.setSpacing(20);
+
+        Label label = new Label();
+        label.setText("Hello, JavaFX");
+        label.setFont(new Font(50));
+
+        Button button = new Button();
+        button.setText("확인");
+        button.setOnAction(event-> Platform.exit());
+        root.getChildren().add(button);
+        root.getChildren().add(label);
+
+        Scene scene = new Scene(root);
+
+        primaryStage.setTitle("AppMain");
+        primaryStage.setScene(scene);
         primaryStage.show();
+
     }
     public static void main(String[] args) {
-        System.out.println(Thread.currentThread().getName()+": main() 호출");
         launch(args);
     }
 }
