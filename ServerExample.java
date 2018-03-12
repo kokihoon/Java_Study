66bdd2d0ad19764e12c79a282303109d522ccc29
kokihoon <rhrlgns777@gmail.com>
2018-03-12 16:07:27 +0900

    socket send data

--- ServerExample.java
+++ ServerExample.java
@@ -1,4 +1,6 @@
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.InetSocketAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -15,6 +17,23 @@ public class ServerExample {
                 Socket socket = serverSocket.accept();
                 InetSocketAddress isa = (InetSocketAddress) socket.getRemoteSocketAddress();
                 System.out.println("[연결 수락함] " + isa.getHostName());
+
+                byte[] bytes = null;
+                String message = null;
+
+                InputStream is = socket.getInputStream();
+                bytes = new byte[100];
+                int readByteCount = is.read(bytes);
+                message = new String(bytes, 0, readByteCount, "UTF-8");
+                System.out.println("데이터 받기 성공 : " + message);
+
+                OutputStream os = socket.getOutputStream();
+                message = "Hello client";
+                bytes = message.getBytes("UTF-8");
+                os.write(bytes);
+                os.flush();
+                System.out.println("[데이터 보내기 성공]");
+
             }
         } catch (Exception e) {
 
