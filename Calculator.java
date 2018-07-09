549a726cb9c332de3fb2e2075875b6087fcd89ed
kokihoon <rhrlgns777@gmail.com>
2018-07-09 16:28:04 +0900

    synchronized Thread study

--- Calculator.java
+++ Calculator.java
@@ -6,7 +6,7 @@ public class Calculator {
         return memory;
     }
 
-    public void setMemory(int memory){
+    public synchronized void setMemory(int memory){
         this.memory = memory;
 
         try {
