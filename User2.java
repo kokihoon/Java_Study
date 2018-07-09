b1379e92b30ef6aa2a78389be0857bbfff942f5a
kokihoon <rhrlgns777@gmail.com>
2018-07-09 16:26:46 +0900

    Thread study

---
+++ User2.java
@@ -0,0 +1,12 @@
+public class User2 extends Thread {
+    private Calculator calculator;
+
+    public void setCalculator(Calculator calculator) {
+        this.calculator = calculator;
+        this.setName("User2");
+    }
+
+    public void run() {
+        calculator.setMemory(50);
+    }
+}
